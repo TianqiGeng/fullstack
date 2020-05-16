c39c6ebd6644d298045dc50e232499517c6ab3d6
Tian Qi Geng <tqgeng@cn.ibm.com>
2020-05-16 18:52:23 +0800

    TS

--- 1_html5/3_JS_Basics/TypeScript/1.js
+++ 1_html5/3_JS_Basics/TypeScript/1.js
@@ -7,12 +7,3 @@ var Hello = /** @class */ (function () {
     return Hello;
 }());
 exports.Hello = Hello;
-function foo(a) {
-    return a + 1;
-}
-foo.bar = 123;
-console.log(foo(foo.bar));
-var ab = { a: 1, b: 1 };
-console.log(ab); //{a:1,b:1}
-var ab2 = { a: 1, b: 1 };
-console.log(ab2); //{a:1,b:1}
